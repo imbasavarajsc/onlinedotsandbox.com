56b98b47f69be4e25036255cb4ad9b2664af1de7
Basavaraj <basuchikkamath5@gmail.com>
2026-09-17 16:44:05 +0530

    chore: update site domain to playdotsandboxes.com and add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dotsandboxesgame.com',
+  site: 'https://playdotsandboxes.com',
   integrations: [sitemap()],
 });
